23c1ae71826e83004dccf423a7187e10e2fc9bd1
davidobsz <davidobszanski@icloud.com>
2022-12-08 15:44:59 +0000

    api was locally stored. Now it is stored externally.

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ openai.Model.retrieve("text-davinci-003")
 app = Flask(__name__)
 
 
-# curl https://api.openai.com/v1/completions -H "Content-Type: application/json" -H "Authorization: Bearer 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" -d '{"model": "text-davinci-003", "prompt": "Say this is a test", "temperature": 0, "max_tokens": 7}'
+
 
 @app.route('/', methods=(["GET", "POST"]))
 def index():
