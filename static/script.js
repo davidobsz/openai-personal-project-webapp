8f21e84b27a5ffd37121340936c9a5a41921765b
davidobsz <davidobszanski@icloud.com>
2022-12-10 01:36:39 +0000

    added "loading" animation

--- static/script.js
+++ static/script.js
@@ -1,9 +1,18 @@
-    var button = document.getElementById("submit-prompt")
-    var prompt = document.getElementById("prompt-input")
-
-    function checkInput(){
-        if (prompt.value == ""){
-            alert("Prompt cannot be empty")
-            return false
-        }
+var button = document.getElementById("submit-prompt")
+var prompt = document.getElementById("prompt-input")
+var loading_element = document.getElementById("load")
+loading_element.style.visibility = "hidden"
+function checkInput(){
+    if (prompt.value == ""){
+        alert("Prompt cannot be empty")
+        return false
     }
+}
+
+function load(){
+    setTimeout( null, 2000);
+    loading_element.style.visibility = "visible"
+    return true
+}
+
+
